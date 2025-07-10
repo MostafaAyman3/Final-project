8619b911bdd6613a1dd824a211f04407699134f9
Yousef Tarek <youseftarek.business@gmail.com>
2025-07-10 04:42:09 +0300

    fix responsive bug

--- src/app/(public)/_components/Hero.tsx
+++ src/app/(public)/_components/Hero.tsx
@@ -4,23 +4,23 @@ import { Bot, Telescope } from "lucide-react";
 
 const Hero = () => {
   return (
-    <section className="relative h-[calc(100vh-53px)] bg-gradient-to-b from-primary/10 to-primary/5 p-2">
-      <div className="container mx-auto h-full flex items-center justify-center">
+    <section className="relative bg-gradient-to-b from-primary/10 to-primary/5 p-2 h-[calc(100vh-53px)]">
+      <div className="flex justify-center items-center mx-auto h-full container">
         <div className="text-center">
-          <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">
+          <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
             Your{" "}
-            <span className="bg-gradient-to-r from-primary to-foreground bg-clip-text text-transparent">
+            <span className="bg-clip-text bg-gradient-to-r from-primary to-foreground text-transparent">
               AI Medical Assistant
             </span>
             <br />
             Fast, Reliable, and Always Available
           </h1>
-          <p className="text-sm md:text-base mt-3 text-muted-foreground text-center max-w-2xl mx-auto">
+          <p className="mx-auto mt-3 max-w-2xl text-muted-foreground text-sm md:text-base text-center">
             Revolutionize your health journey with AI-powered diagnostics. From
             skin and eyes to brain scans and lab results, our smart platform
             delivers instant medical insights—anytime, anywhere.
           </p>
-          <div className="mt-4 flex items-center justify-center gap-2">
+          <div className="flex lg:flex-row flex-col justify-center items-center gap-2 mt-4">
             <Button size="lg">
               <Telescope /> Start a Diagnosis Now
             </Button>
@@ -33,7 +33,7 @@ const Hero = () => {
       </div>
 
       <Particles
-        className="absolute inset-0 z-0"
+        className="z-0 absolute inset-0"
         quantity={110}
         ease={100}
         size={1}
